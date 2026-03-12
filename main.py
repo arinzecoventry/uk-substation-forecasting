f6da5c68df253b26fdf43b39419652c50847ea4c
arinzecoventry <arinzec@coventry.ac.uk>
2026-03-12 18:30:25 +0000

    added basic data loading function

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import pandas as pd
 
 DATASETS = {
     "511285_Oakwood_Rd_Llanedeyrn": "511285.csv",
@@ -10,4 +11,20 @@ OUTPUT_DIR = "project_results"
 CLEAN_DIR = os.path.join(OUTPUT_DIR, "cleaned_data")
 
 os.makedirs(OUTPUT_DIR, exist_ok=True)
-os.makedirs(CLEAN_DIR, exist_ok=True) 
+os.makedirs(CLEAN_DIR, exist_ok=True)
+
+def load_and_clean_substation(file_path, dataset_name):
+    df = pd.read_csv(file_path)
+    df["Timestamp"] = pd.to_datetime(df["Timestamp"])
+    df_kw = df[df["Units"] == "kW"].copy()
+    
+    series = (
+        df_kw.groupby("Timestamp")["Value"]
+        .sum()
+        .sort_index()
+        .to_frame(name="load_kw")
+    )
+    
+    clean_path = os.path.join(CLEAN_DIR, f"{dataset_name}_cleaned.csv")
+    series.to_csv(clean_path, index_label="Timestamp")
+    return series
