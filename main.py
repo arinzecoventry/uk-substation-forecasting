5bf891bbf77f4fd5dfc5b8f466a493c7a7bd8c37
SebastienWolfie <malseasers@gmail.com>
2026-03-13 14:20:12 +0000

    implemented evaluation metrics MAE and RMSE

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import os
 import pandas as pd
+import numpy as np
+from math import sqrt
+from sklearn.metrics import mean_absolute_error, mean_squared_error
 
 DATASETS = {
     "511285_Oakwood_Rd_Llanedeyrn": "511285.csv",
@@ -37,3 +40,9 @@ def load_and_clean_substation(file_path, dataset_name):
     clean_path = os.path.join(CLEAN_DIR, f"{dataset_name}_cleaned.csv")
     series.to_csv(clean_path, index_label="Timestamp")
     return series
+
+
+def evaluate_forecast(y_true, y_pred):
+    mae = mean_absolute_error(y_true, y_pred)
+    rmse = sqrt(mean_squared_error(y_true, y_pred))
+    return mae, rmse
