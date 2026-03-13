5c0a1c4532a095f299ac65ec11cf644b10f05bca
arinzecoventry <arinzec@oventry.a.uk>
2026-03-13 23:11:31 +0000

    fix: ensured timestamps are UTC and added interpolation

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ os.makedirs(CLEAN_DIR, exist_ok=True)
 
 def load_and_clean_substation(file_path, dataset_name):
     df = pd.read_csv(file_path)
-    df["Timestamp"] = pd.to_datetime(df["Timestamp"])
+    df["Timestamp"] = pd.to_datetime(df["Timestamp"], utc=True)
     df_kw = df[df["Units"] == "kW"].copy()
     
     series = (
@@ -27,6 +27,15 @@ def load_and_clean_substation(file_path, dataset_name):
         .sort_index()
         .to_frame(name="load_kw")
     )
+    
+    full_index = pd.date_range(
+        start=series.index.min(),
+        end=series.index.max(),
+        freq="10min",
+        tz="UTC"
+    )
+    series = series.reindex(full_index)
+    series["load_kw"] = series["load_kw"].interpolate(method="time")
     
     clean_path = os.path.join(CLEAN_DIR, f"{dataset_name}_cleaned.csv")
     series.to_csv(clean_path, index_label="Timestamp")
